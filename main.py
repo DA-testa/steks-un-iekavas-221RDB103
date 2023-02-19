f59d3346de2f07f65c1f85db50ccee0c305383e7
221RDB103 <125796518+221RDB103@users.noreply.github.com>
2023-02-19 16:25:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# python3
-
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -13,18 +11,26 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(Bracket(next, i+1))
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i+1
+            left_bracket = opening_brackets_stack.pop()
+            if not are_matching(left_bracket.char, next):
+                return i+1
+    if opening_brackets_stack:
+        left_bracket = opening_brackets_stack.pop()
+        return left_bracket.position
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    if mismatch == "Success":
+        print(mismatch)
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
